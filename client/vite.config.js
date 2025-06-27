61eaf292717be4f41ab5ae86aa6da94125f5baf5
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-06-28 01:57:49 +0300

    render debug fixvite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: [
+      'forex-bureau-ui.onrender.com',
+      'forex-bureau-api.onrender.com'
+    ]
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  }
 })
